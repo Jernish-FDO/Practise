cdef1319bd53464eac2f9eebff3a20b08548aa31
Jernish <111893985+Jernish-FDO@users.noreply.github.com>
2025-12-27 00:45:30 +0530

    Fix tailwind config - remove missing plugin dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -139,9 +139,5 @@ export default {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+  plugins: [],
 }
